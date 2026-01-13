3379e04f3b9f581da34997a4e939b70f4a607e11
kinachouhan <chouhankina@gmail.com>
2026-01-14 03:18:04 +0530

    feat: add initial food item model and routes

---
+++ Backend/models/foodItemModel.js
@@ -0,0 +1,30 @@
+
+// import mongoose from "mongoose"
+
+// const foodItemSchema = new mongoose.Schema({
+//      name:{
+//          type: String,
+//          required: true
+//      },
+//      price: {
+//          type: Number,
+//          required: true
+//      },
+//      image: {
+
+//      },
+//      category:{
+//          type: 
+//      }
+// })
+
+
+
+
+
+
+//    name: "Home Style North Indian Thali",
+//       price: 199,
+//       image:
+//          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/5/29/b05cd867-6caf-4dfb-a335-9fff0c182cbe_80651.jpg",
+//       category: "Breakfast",
